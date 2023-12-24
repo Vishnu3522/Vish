bf4c6a610b813704af613b48ce04ae2f28d3f651
Vishnu3522 <vishnukumar.it24@jecrc.ac.in>
2023-12-24 18:15:21 +0530

    added-routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './Components/Navbar';
 import { Routes, Route } from "react-router-dom";
 import Home from './Components/Home';
+import Login from "./Login";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </div>
   );
